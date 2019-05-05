6ab97559980b62b625381a29f7637e84357c2984
storres8 <stevetorres7562@gmail.com>
2019-05-05 11:12:46 -0400

    updated,

--- webserver/src/app.js
+++ webserver/src/app.js
@@ -2,10 +2,25 @@ const express = require("express");
 
 const app = express();
 
+// Setting up express urls
+// req and res has to be in this order for the path to work
 app.get("", (req, res) => {
   res.send("Hello from express");
 });
 
+app.get("/about", (req, resp) => {
+  resp.send("Welcome to the about page!");
+});
+
+app.get("/about", (req, resp) => {
+  resp.send("Welcome to the about page!");
+});
+
+app.get("/weather", (req, resp) => {
+  resp.send("Weather Page!");
+});
+
+// Setting up localhost port
 app.listen(3000, () => {
   console.log("listening on port 3000...");
 });
