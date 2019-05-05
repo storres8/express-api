b194f6d3fe503a3e3ed1fbc6b20d8aaf5510f1c0
storres8 <stevetorres7562@gmail.com>
2019-05-05 12:33:20 -0400

    rendered urls for different parts of weather app.

--- webserver/src/app.js
+++ webserver/src/app.js
@@ -1,26 +1,35 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
+const htmlHome = path.join(__dirname, "../public");
+const htmlAbout = path.join(__dirname, "../public/about.html");
+const htmlHelp = path.join(__dirname, "../public/help.html");
+
 // Setting up express urls
+
+app.use(express.static(htmlHome));
+
 // req and res has to be in this order for the path to work
-app.get("", (req, res) => {
-  res.send("Hello from express");
+app.get("/help", (req, res) => {
+  res.sendFile(htmlHelp);
 });
 
 app.get("/about", (req, resp) => {
-  resp.send("Welcome to the about page!");
-});
-
-app.get("/about", (req, resp) => {
-  resp.send("Welcome to the about page!");
+  resp.sendFile(htmlAbout);
 });
 
 app.get("/weather", (req, resp) => {
-  resp.send("Weather Page!");
+  resp.send({
+    place: "New York City",
+    temperature: "69F",
+    climate: "Raining"
+  });
 });
 
 // Setting up localhost port
 app.listen(3000, () => {
+  // eslint-disable-next-line no-console
   console.log("listening on port 3000...");
 });
